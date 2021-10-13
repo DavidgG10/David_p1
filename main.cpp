52eb0dedce3eec47dc5fcc1b661deeb7f9a44675
DavidG <72959512+DavidgG10@users.noreply.github.com>
2021-10-13 15:42:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Eduardo "; //Se pone el primer nombre
+  cout << "David ";// Se pone el segundo nombre 
+  cout << "González ";// Se pone el primer apellido
+  cout << "González "; // Se pone el segundo apellido
+  cout << "10-2 B"; // Se pone la sección
+}
